9025c6df0821b4b8a07ac4055b01c1330b08178f
Alizwell <844534780@qq.com>
2018-07-10 16:24:29 +0800

    添加webpack相关配置

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,3 +1,4 @@
+//获取配置文件相关的参数
 var  config = require('../config');
 
 if( !process.env.NODE_ENV ) process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
@@ -9,8 +10,70 @@ var  opn = require('opn')
 var  proxyMiddleware = require('http-proxy-middleware')	
 var  webpackConfig = require('./webpack.dev.conf')
 
-var  post = process.env.PORT ||  config.dev.root
+var  port = process.env.PORT ||  config.dev.port
 
 var  server = express()
 
 var  compiler = webpack(webpackConfig)
+
+var  devMiddleware = require('webpack-dev-middleware')(compiler,{
+	publicPath:webpackConfig.output.publicPath,
+	stats:{
+		colors:true,
+		chunks:false
+	}
+})
+
+var  hotMiddleware = require('webpack-hot-middleware')(compiler);
+
+compiler.plugin('compilation',function(compilation){
+	compilation.plugin('html-webpack-plugin-after-emit',function(data,cb){
+		hotMiddleware.publish({
+			action:'reload'
+		})
+		cb()
+	})
+})
+
+var  context = config.dev.context
+
+switch(process.dev.NODE_ENV){
+	case 'local':var proxypath = 'http://localhost:8081';break;
+	case 'online':var proxypath = 'http://elm.cangdu.org';break;
+	default: var proxypath = config.dev.proxypath;
+}
+
+
+var  options = {
+	target:proxypath,
+	changeOrigin:true
+}
+
+if(context.length){
+	server.use(proxyMiddleware(context, options))
+}
+
+server.use(require('connect-history-api-fallback')())
+
+server.use(devMiddleware)
+
+server.use(hotMiddleware)
+
+
+var  staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+
+server.use(staticPath, express.static('./static'))
+
+module.exports = server.listen(port, function(err){
+	if(err){
+		console.log(err)
+		return
+	}
+
+	var  uri = 'http://localhost:' + port
+	console.log('Listening at ' + uri + '\n')
+
+	if(process.env.NODE_ENV !== 'testing'){
+		opn(uri)
+	}
+})
--- config/index.js
+++ config/index.js
@@ -4,11 +4,37 @@ module.exports = {
 	build:{
 		env:{
 			NODE_ENV: '"production"'
-		}
+		},
+		index: path.resolve(__dirname, '../elm/index.html'),
+		assetsRoot:path.resolve(__dirname, '../elm'),
+		assetsSubDiectory:'static',
+		assetsPublicPath:'/elm/',
+		productionSourceMap: true,
+		productionGzip: false,
+		productionGzipExtensions:['js','css']
 	},
 	dev:{
 		env:{
 			NODE_ENV:'"development"'
-		}
+		},
+		port:8000,
+		assetsSubDiectory:'static',
+		assetsPublicPath:'/',
+		context:[
+			'/shopping',
+            '/ugc',
+            '/v1',
+            '/v2',
+            '/v3',
+            '/v4',
+            '/bos',
+            '/member',
+            '/promotion',
+            '/eus',
+            '/payapi',
+            '/img',
+		],
+		proxypath:'http://cangdu.org:8001',
+		cssSourceMap:false
 	}
 }
