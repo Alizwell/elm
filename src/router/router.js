b9e6b2e5d810807fc94b992b8d14e1b0a8c436c1
Alizwell <844534780@qq.com>
2018-07-10 18:43:20 +0800

    开始第一个页面的编写，目前环境还有问题，需要进行修改

--- src/router/router.js
+++ src/router/router.js
@@ -1,1 +1,23 @@
-router.js
+import  App  from  '../app.vue'
+
+var  home = r => require.ensure([],()=>r(require('../page/home/home')),'home')
+var  login = r => require.ensure([],()=>r(require('../page/login/login')),'login')
+
+export  default [{
+	path:'/',
+	component:App,
+	children:[
+		{
+			path:'',
+			redirect:'/home'
+		},
+		{
+			path:'/home',
+			component:home
+		},
+		{
+			path:'/login',
+			component:login
+		}
+	]
+}]
